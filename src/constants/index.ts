efb6f0779ca040dd3b4deb8887384ef8a7034adb
stasionok <stasionok@gmail.com>
2020-09-30 23:10:41 +0300

    ropsten contracts and configs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x9ca951fddc636b0274a511A40aF3Ea898aa9BCDc'
 
 export const EXPLORER_ADDRESS = 'etherscan.io';
 
